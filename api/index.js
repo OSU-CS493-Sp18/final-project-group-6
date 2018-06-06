ba7a3cd7e8e1d18b5280f047faaae4c4241d10b4
Bradley Imai <imaib@onid.oregonstate.edu>
2018-06-06 15:52:14 -0700

    uncommenting this line to allow use

--- api/index.js
+++ api/index.js
@@ -3,4 +3,4 @@ const router = module.exports = require('express').Router();
 router.use('/beer', require('./beer').router);
 //router.use('/reviews', require('./reviews').router);
 //router.use('/manufacturer', require('./manufacturer').router);
-//router.use('/users', require('./users').router);
+router.use('/users', require('./users').router);
