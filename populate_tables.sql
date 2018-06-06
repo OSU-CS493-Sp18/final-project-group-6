65dcb37a494484c1fe48222fd7f37098f18e3d7c
Bradley Imai <imaib@onid.oregonstate.edu>
2018-06-06 14:47:34 -0700

    added in beer listings

--- populate_tables.sql
+++ populate_tables.sql
@@ -17,4 +17,17 @@ CREATE TABLE beers (
 -- Insert initial values into businesses table --
 INSERT INTO beers (manufacturerid, name, abv, ibu, calories, type)
 VALUES
-('0', 'Pacifico', '5', '6', '146', 'pilsner');
+('0', 'Pacifico', '5.4', '6', '146', 'Pilsner'),
+('1', 'Fat Tire', '5.2', '22', '160', 'Belgian Style Ale'),
+('2', 'VooDoo Ranger', '7.0', '50', '160', 'IPA'),
+('3', 'Citradelic', '6', '50', '170', 'Tangerine IPA'),
+('4', 'Juice Haze IPA', '7.2', '42', '230', 'IPA'),
+('5', 'Coors Light', '4.2', '10', '102', 'Lager'),
+('6', 'Guiness', '4.2', '45', '125', 'Stout'),
+('7', 'Kiwanda', '5.4', '25', '125', 'Cream Ale'),
+('8', 'Umbrella', '7.4', '60', '160', 'Pale Ale'),
+('9', 'Tsunami', '7', '45', '170', 'stout'),
+
+
+
+
